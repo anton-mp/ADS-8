d69ef20acae39db478985f715b901593449eff26
anton-mp <abramova2712katya@gmail.com>
2026-05-30 13:45:47 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -2,4 +2,73 @@
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
 
+#include <algorithm>
+
+template <typename T>
+class BST
+{
+ public:
+    struct Node {
+        T data;
+        int count;
+        Node* left;
+        Node* right;
+    };
+ private:
+     Node* root;
+     Node* addNode(Node* root, const T& data) {
+         if (root == nullptr) {
+             root = new Node;
+             root->data = data;
+             root->count = 1;
+             root->left = nullptr;
+             root->right = nullptr;
+         }
+         else if (data > root->data)
+             root->right = addNode(root->right, data);
+         else if (data < root->data)
+             root->left = addNode(root->left, data);
+         else
+             root->count++;
+         return root;
+     }
+     void deleteTree(Node* root) {
+         if (root == nullptr)
+             return;
+         deleteTree(root->left);
+         deleteTree(root->right);
+         delete root;
+     }
+     int treeDepth(Node* root) {
+         if (root == nullptr)
+             return -1;
+         return 1 + std::max(treeDepth(root->left), treeDepth(root->right));
+     }
+     int searchTree(Node* root, const T& data) {
+         if (root == nullptr)
+             return 0;
+         if (data < root->data) {
+             return searchTree(root->left, data);
+         }
+         if (data > root->data) {
+             return searchTree(root->right, data);
+         }
+         return root->count;
+     }
+ public:
+     BST() : root(nullptr) {}
+     ~BST(){
+         deleteTree(root);
+     }
+     void insert(const T& data) {
+         root = addNode(root, data);
+     }
+     int depth() {
+         return treeDepth(root);
+     }
+     int search(T value) {
+         return searchTree(root, value);
+     }
+};
+
 #endif  // INCLUDE_BST_H_
